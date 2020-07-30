62dcf1f03cde6b44f20390b2a8ebf386033436bc
William Lee <wlee753159@gmail.com>
2020-07-30 22:04:33 +0100

    Adjust RGB weighting

--- color.py
+++ color.py
@@ -84,7 +84,7 @@ COLORS = {
     "black_terracotta": ((26, 15, 11), (31, 18, 13), (37, 22, 16)),
 }
 
-COLOR_WEIGHTS = (0.47, 0.29, 0.24, 0.15)
+COLOR_WEIGHTS = (0.47, 0.29, 0.24, 0.2)
 
 # Maps RGB color to most suitable block and height difference
 COLOR_CACHE = {}
